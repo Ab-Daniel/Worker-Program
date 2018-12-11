036c3f59faab0e412448871a423bfa1b75a0b2d0
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2018-12-10 17:43:46 -0700

    Create WorkerTest.java

---
+++ WorkerTest.java
@@ -0,0 +1,10 @@
+
+public class WorkerTest 
+{
+	
+	SalariedWorker sWorker = new SalariedWorker("Sally", (double) 45);
+	HourlyWorker hWorker = new HourlyWorker("Harry", (double) 35);
+	
+	//System.out.println("The salaried worker works at a $45 dollar rate");
+	
+}
